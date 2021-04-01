60ef978259aedbb1166657528647e86930ff9fe2
prabhkiratbits <prabhkirat293@gmail.com>
2021-04-01 23:41:11 +0530

    Update by Prabhkirat

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -2,6 +2,7 @@ public class HelloWorld {
 
     public static void main(String args) {
         System.out.println("Hello this is DevOps Assignment1");
+        System.out.println("This is an update made by Prabhkirat");
     }
 
 }
